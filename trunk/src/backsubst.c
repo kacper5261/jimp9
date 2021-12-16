e44e701ce557670b3d4259dab3a4f1457c7f08ea
Szkomprompa <96201043+Szkomprompa@users.noreply.github.com>
2021-12-16 23:19:33 +0100

    Update backsubst.c

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -7,21 +7,26 @@
  */
  
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-    int j, i;
+	int j, i;
 
-    if (mat->r != mat->c)
-        return 2;
+	for (i = 0; i < mat->r; i++){
+		if (mat->data[i][i] == 0)
+			return 1;
+	}
 
-    for(j = (mat->r) - 1; j >= 0; j--){
-        double tmp = b->data[j][0];
-        for (i = (mat->c) - 1; i > 0; i--){
-            if (j < i)
-                tmp = tmp - mat->data[j][i] * x->data[i][0];
-        }
+	if (mat->r != mat->c)
+		return 2;
+
+	for(j = (mat->r) - 1; j >= 0; j--){
+		double tmp = b->data[j][0];
+		for (i = (mat->c) - 1; i > 0; i--){
+			if (j < i)
+				tmp = tmp - mat->data[j][i] * x->data[i][0];
+		}
     if (mat->data[j][j] == 0)
       return 1;
-        x->data[j][0] = tmp / mat->data[j][j];
-    }
+		x->data[j][0] = tmp / mat->data[j][j];
+	}
 
-    return 0;
+	return 0;
 }
