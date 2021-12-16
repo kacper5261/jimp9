7880011c1b4e289569c9ae88fe056791e213c8c1
kacper5261 <95921668+kacper5261@users.noreply.github.com>
2021-12-16 23:09:14 +0100

    Update gauss.h

--- trunk/src/gauss.h
+++ trunk/src/gauss.h
@@ -8,5 +8,8 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b);
+void zerowanie(int a, Matrix *mat, Matrix *b);
+int potega(int a, int b);
+double wyznacznik(Matrix *mat);
 
 #endif
